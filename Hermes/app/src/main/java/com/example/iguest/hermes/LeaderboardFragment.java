cc892f96a5d6b3a11dbd9cfc433c2e0ce89d1fd9
Bruce Ng <bng@uievolution>
2016-03-08 11:43:05 -0800

    leaderboard should work

--- Hermes/app/src/main/java/com/example/iguest/hermes/LeaderboardFragment.java
+++ Hermes/app/src/main/java/com/example/iguest/hermes/LeaderboardFragment.java
@@ -3,15 +3,28 @@ package com.example.iguest.hermes;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LeaderboardFragment extends Fragment {
+    private ArrayAdapter adapter;
 
 
     public LeaderboardFragment() {
@@ -23,8 +36,25 @@ public class LeaderboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        ArrayList<User> list = new ArrayList<>();
+        adapter = new ArrayAdapter<User>(getActivity(), R.layout.list_item, list);
+        AdapterView listView = (AdapterView) rootView.findViewById(R.id.leaderView);
+        listView.setAdapter(adapter);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+        query.orderByDescending("score").setLimit(200);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    for (ParseObject object : objects) {
+                        User a = new User(object.getString("screenName"), object.getString("phoneNumber"), object.getInt("score"));
+                        adapter.add(a);
+                    }
+                }
+            }
+        });
+
+        return rootView;
     }
-
-
 }
